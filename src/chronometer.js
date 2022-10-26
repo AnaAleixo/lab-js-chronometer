254e367755bfbf4c59ea05bc10783bea2ed8ac5b
Ana Cláudia L Aleixo <analima0608@gmail.com~>
2022-10-25 21:00:55 -0300

    é peroba

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,50 @@
 class Chronometer {
   constructor() {
+      this.currentTime = 0;
+      this.intervalId = null;
+    }
     // ... your code goes here
   }
 
   start(printTimeCallback) {
+    this.intervalId = setInterval(() =>{
+      this.currenTime++;
+      if(printTimeCallback){
+        printTimeCallback();
+
+      }
+    },100)
     // ... your code goes here
   }
 
   getMinutes() {
+    return Math.floor(this.currentTime)
     // ... your code goes here
   }
 
   getSeconds() {
+    return this.currentTime%60
     // ... your code goes here
   }
 
   computeTwoDigitNumber(value) {
+    return(`0` + value).slice(-2,);
     // ... your code goes here
   }
 
   stop() {
+    return clearInterval(this.intervalId)
     // ... your code goes here
   }
 
   reset() {
+   return this.currentTime =0;
     // ... your code goes here
   }
 
   split() {
+  return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`
+  
     // ... your code goes here
   }
 }
